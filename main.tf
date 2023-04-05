e49519b88740a72c205dbe5d6419ac923f6d27fc
vsiarif <129980192+vsiarif@users.noreply.github.com>
2023-04-06 01:19:05 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,14 @@
 terraform {
   required_providers {
-    aws = {
-      source = "hashicorp/aws"
+    # This is the current syntax, which is still supported
+    random = ">= 2.7.0"
+
+    # This is the new syntax. "source" and "version" are both 
+    # optional, though in the future "source" will be required for 
+    # any provider that isn't maintained by HashiCorp.
+    random = {
+        source = "registry.terraform.io/hashicorp/random"
+        version = "2.1.0"
     }
   }
 }
