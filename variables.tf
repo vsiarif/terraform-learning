582f012c115eab14c6ff712495c411c6bb57868e
vsiarif <129980192+vsiarif@users.noreply.github.com>
2023-04-06 01:29:50 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,14 +1,36 @@
-variable "network_interface_id" {
-  type = string
-  default = "network_id_from_aws"
+variable "access_key" {
+        description = "AKIAQUMQAZ7ZY3RER5OV"
+}
+variable "secret_key" {
+        description = "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"
 }
 
-variable "ami" {
-    type = string
-    default = "ami-005e54dee72cc1d00"
+
+variable "instance_name" {
+        description = "Name of the instance to be created"
+        default = "awsbuilder-demo"
 }
 
 variable "instance_type" {
-    type = string
-    default = "t2.micro"
+        default = "t2.micro"
+}
+
+variable "subnet_id" {
+        description = "The VPC subnet the instance(s) will be created in"
+        default = "subnet-07ebbe60"
+}
+
+variable "ami_id" {
+        description = "The AMI to use"
+        default = "ami-09d56f8956ab235b3"
+}
+
+variable "number_of_instances" {
+        description = "number of instances to be created"
+        default = 1
+}
+
+
+variable "ami_key_pair_name" {
+        default = "tomcat"
 }
