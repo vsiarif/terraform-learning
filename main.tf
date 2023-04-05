a05e5673adae5d1530c5ab16726f80e8f164bb14
vsiarif <129980192+vsiarif@users.noreply.github.com>
2023-04-06 02:50:58 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,9 @@
 # Configure the AWS Provider
 provider "aws" {
+  access_key = "AKIAQUMQAZ7ZY3RER5OV"
+  Secret_key = "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"
   region = "Global"
+  version = "~> 4.0"
 }
 resource "aws_instance" "ec2_instance" {
     ami = "${var.ami_id}"
