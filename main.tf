723107717e0a19864b29e9685ba5a7d0c6cf40d0
vsiarif <129980192+vsiarif@users.noreply.github.com>
2023-04-06 01:21:10 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,16 +12,3 @@ terraform {
     }
   }
 }
-resource "aws_instance" "" {
-  ami           = var.ami
-  instance_type = var.instance_type
-
-  network_interface {
-    network_interface_id = var.network_interface_id
-    device_index         = 0
-  }
-
-  credit_specification {
-    cpu_credits = "unlimited"
-  }
-}
