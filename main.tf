f4048b0209d9a32135168111bc4a0c481bef20ca
vsiarif <129980192+vsiarif@users.noreply.github.com>
2023-04-06 01:26:20 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,3 +9,10 @@ terraform {
     }
   }
 }
+resource "aws_instance" "ec2_instance" {
+    ami = "${var.ami_id}"
+    count = "${var.number_of_instances}"
+    subnet_id = "${var.subnet_id}"
+    instance_type = "${var.instance_type}"
+    key_name = "${var.ami_key_pair_name}"
+} 
