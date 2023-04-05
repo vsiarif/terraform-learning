23988b24f6ca556ac66b613aafbb5eb82eb4fef7
vsiarif <129980192+vsiarif@users.noreply.github.com>
2023-04-06 01:22:35 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,6 @@
 terraform {
   required_providers {
-    # This is the current syntax, which is still supported
-    random = ">= 2.7.0"
-
-    # This is the new syntax. "source" and "version" are both 
+        # This is the new syntax. "source" and "version" are both 
     # optional, though in the future "source" will be required for 
     # any provider that isn't maintained by HashiCorp.
     random = {
