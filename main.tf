e536d42ddbce4bfd9e0b6adbcafcdd2d6dadac78
vsiarif <129980192+vsiarif@users.noreply.github.com>
2023-04-06 13:10:57 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,7 @@
 # Configure the AWS Provider
 provider "aws" {
-  access_key=var.aws_access_key
-  secret_key=var.aws_secret_key
-  region = "us-east-1"
- 
-}
+    region = "us-east-1"
+ }
 resource "aws_instance" "ec2_instance" {
     ami = "${var.ami_id}"
     count = "${var.number_of_instances}"
