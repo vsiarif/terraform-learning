0d7e0a8d054e6fad1c723dbce8b12edd154b9448
vsiarif <129980192+vsiarif@users.noreply.github.com>
2023-04-06 13:15:10 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,7 @@
 # Configure the AWS Provider
 provider "aws" {
+    access_key = $"var.AWS_ACCESS_KEY_ID"
+    secret_key = $"var.AWS_SECRET_ACCESS_KEY"
     region = "us-east-1"
  }
 resource "aws_instance" "ec2_instance" {
