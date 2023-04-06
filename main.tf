a861d43c9b45464cd5b9a5217be098f27cd51808
vsiarif <129980192+vsiarif@users.noreply.github.com>
2023-04-06 13:32:20 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,8 @@
 # Configure the AWS Provider
 provider "aws" {
-    access_key = "AKIAQUMQAZ7ZY3RER5OV"
-    secret_key = "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"
+    iam_user_name = "tfuser"
+    iam_access_key_id = "AKIAQUMQAZ7ZY3RER5OV"
+    iam_access_key_secret = "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"
     region = "us-east-1"
  }
 resource "aws_instance" "ec2_instance" {
